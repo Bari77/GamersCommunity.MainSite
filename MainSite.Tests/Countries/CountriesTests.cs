bd8adb129fc5a1d1a5e97887013204b44f85a28c
Bari <alex.c9777700@gmail.com>
2025-10-02 16:14:45 +0200

    Add tests

---
+++ MainSite.Tests/Countries/CountriesTests.cs
@@ -0,0 +1,43 @@
+﻿using GamersCommunity.Core.Tests;
+using MainSite.Consumer.Services;
+using MainSite.Database.Context;
+using MainSite.Database.Models;
+
+namespace MainSite.Tests.Countries
+{
+    public class CountriesTests : GenericServiceTests<GamersCommunityDbContext, CountriesService, Country>, IClassFixture<TestsFixture>
+    {
+        public CountriesTests(TestsFixture fixture) : base(fixture._countriesService)
+        {
+            fixture._testDbContext.AddRange(GetFakeData());
+            fixture._testDbContext.SaveChanges();
+        }
+
+        protected override List<Country> GetFakeData()
+        {
+            return
+            [
+                new() {
+                    Id = 1,
+                    CreationDate = DateTime.Now,
+                    ModificationDate = DateTime.Now,
+                    Name = "Country 1",
+                },
+                new() {
+                    Id = 2,
+                    CreationDate = DateTime.Now,
+                    ModificationDate = DateTime.Now,
+                    Name = "Country 2",
+                },
+            ];
+        }
+
+        protected override Country GetNewEntity()
+        {
+            return new Country
+            {
+                Name = "Country test"
+            };
+        }
+    }
+}
---
+++ MainSite.Tests/TestsFixture.cs
@@ -0,0 +1,55 @@
+﻿using MainSite.Consumer.Services;
+using MainSite.Database.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace MainSite.Tests
+{
+    public class TestsFixture : IDisposable
+    {
+        #region Global
+
+        /// <summary>
+        /// Testing database context
+        /// </summary>
+        public readonly GamersCommunityDbContext _testDbContext;
+
+        #region Implementations
+
+        #region Countries
+
+        /// <summary>
+        /// Implementation for countries
+        /// </summary>
+        public readonly CountriesService _countriesService;
+
+        #endregion
+
+        #endregion
+
+        #endregion
+
+        /// <summary>
+        /// Called when tests start
+        /// /!\ CARE: This context is shared between tests /!\
+        /// </summary>
+        public TestsFixture()
+        {
+            var options = new DbContextOptionsBuilder<GamersCommunityDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            _testDbContext = new GamersCommunityDbContext(options);
+
+            // Init all implementations
+            _countriesService = new CountriesService(_testDbContext);
+        }
+
+        /// <summary>
+        /// Called when test is ended
+        /// </summary>
+        public void Dispose()
+        {
+            // Clean
+        }
+    }
+}
